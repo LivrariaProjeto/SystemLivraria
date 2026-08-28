95f367f6a86ca59940d0e3a33e30ec3246346c5b
ArthurMattenhauerSAYMYNAME <arthurzera.07@gmail.com>
2026-08-28 19:54:23 -0300

    Adiciona elementos básicos no form do login

--- SystemLivraria/SystemLivraria/forms/frmLogon.cs
+++ SystemLivraria/SystemLivraria/forms/frmLogon.cs
@@ -16,5 +16,42 @@ namespace SystemLivraria.forms
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogon_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_esqueceu_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            lbl_esqueceu.LinkVisited = true;
+
+            
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://youtu.be/HNCgBuI2eJc?si=VBefxVpJMWIyRKD4",
+                UseShellExecute = true
+            });
+        }
     }
 }
--- SystemLivraria/SystemLivraria/forms/frmSplashScreen.Designer.cs
+++ SystemLivraria/SystemLivraria/forms/frmSplashScreen.Designer.cs
@@ -36,9 +36,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(93, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(108, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(351, 276);
+            this.pictureBox1.Size = new System.Drawing.Size(312, 276);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
