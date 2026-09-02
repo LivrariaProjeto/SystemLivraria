1c3588da833452884f971d233fd8786b53f034d1
sofiaarantes1707-spec <sofiaarantes1707@gmail.com>
2026-09-01 21:35:59 -0300

    Fiz o login funcionar

---
+++ SystemLivraria/SystemLivraria/data/DataSet2.cs
@@ -0,0 +1,23 @@
+﻿namespace SystemLivraria.data
+{
+
+
+    partial class DataSet2
+    {
+        partial class UsuariosDataTable
+        {
+        }
+    }
+}
+//GetData() -> para retornar todos os registros da tabela 
+//GetDataBy() -> para retornar um registro específico da tabela -> GetDataBy(@Login, @Senha)
+//Fill() -> para preencher um DataTable com todos os registros da tabela -> para incluir algo em uma tabela já existente
+//Login -> vamos usar o GetDataBy(), porque queremos receber o resultado da consulta e verificar se encontrou um usuário.
+namespace SystemLivraria.data.DataSet2TableAdapters
+{
+
+
+    public partial class UsuariosTableAdapter
+    {
+    }
+}
--- SystemLivraria/SystemLivraria/forms/frmLogon.cs
+++ SystemLivraria/SystemLivraria/forms/frmLogon.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SystemLivraria.data.DataSet2TableAdapters;
 
 namespace SystemLivraria.forms
 {
@@ -34,8 +35,43 @@ namespace SystemLivraria.forms
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string usuario = textBox1.Text;
+            string senha = textBox2.Text;
+            //criando um objeto do tipo UsuariosTableAdapter para acessar os dados da tabela de usuários
+            //ele possui métodos para preencher um DataTable com os dados da tabela de usuários,
+            //como para inserir, atualizar e excluir registros -> o fill, o get, o fillby e o getby
+            //cada metodo possui a consulta sql correspondente -> exemplo: o método GetDataBy possui
+            //"SELECT * FROM Usuarios WHERE Login_usu = @Login AND Senha_usu = @Senha"
+            //o nosso objeto vai executar a consulta sql e retornar os dados da tabela
+            //de usuários que correspondem ao login e senha digitados
+            // o objeto vai executar o que ele faz (TableAdapter -> liga as tabelas com o codigo c#)
+            // mais o método ligado a ele (GetDataBy) e vai retornar os dados da tabela de
+            // usuários que correspondem ao login e senha digitados
+            UsuariosTableAdapter tableAdap = new UsuariosTableAdapter();
+            //o resultado vai vir em forma de DataTable, que é uma tabela em memória 
 
+            if (usuario == "" || senha == "")
+            {
+                MessageBox.Show("Preencha todos os campos!");
+            } 
+            else
+            {
+                DataTable result = tableAdap.GetDataBy(usuario, senha);
+                if(result.Rows.Count > 0)//se achar o usuario verifique a senha
+                {
+                        MessageBox.Show("Login realizado com sucesso!", "    LOGIN REALIZADO!", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                        //shów é o método que possui a classe MessageBox,
+                        //que exibe uma caixa de diálogo com uma mensagem para o usuário
+                        //o método Show possui vários parâmetros, como o texto da mensagem,
+                        //o título da caixa de diálogo, os botões que serão exibidos e o ícone que será exibido
+                }
+                else
+                {
+                    MessageBox.Show("Senha ou Login incorretos!", "    ERRO", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+            }   
         }
+            
 
         private void frmLogon_Load(object sender, EventArgs e)
         {
